e39b7fa4c55d9076ab3ed20513a5132b40ef862f
berkay <66202981+berkay-yalin@users.noreply.github.com>
2023-03-09 12:22:47 +0000

    poisson distributions template

---
+++ python_probabilities/distributions_poisson.py
@@ -0,0 +1,30 @@
+def PoissonPD_validate():
+    pass
+
+def PoissonPD_calculate():
+    pass
+
+def PoissonPD():
+    pass
+
+
+
+def PoissonCD_validate():
+    pass
+
+def PoissonCD_calculate():
+    pass
+
+def PoissonCD():
+    pass
+
+
+
+def InvPoissonCD_validate():
+    pass
+
+def InvPoissonCD_calculate():
+    pass
+
+def InvPoissonCD():
+    pass
