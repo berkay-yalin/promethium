f471ab8314126d28b4db572648e0e00994a7cda7
berkay-yalin <66202981+berkay-yalin@users.noreply.github.com>
2023-03-10 01:03:37 +0000

    PoissonPD function

--- python_probabilities/distributions_poisson.py
+++ python_probabilities/distributions_poisson.py
@@ -1,11 +1,20 @@
-def PoissonPD_validate():
-    pass
+from fractions import Fraction
+import math
 
-def PoissonPD_calculate():
-    pass
+def PoissonPD_validate(k: object, p: object) -> None:
+	if not isinstance(k, int):
+		raise TypeError("Input value k must be integer")
+	if not isinstance(p, (int, float)):
+		raise TypeError("Input value p is invalid")
+	if p <= 0:
+		raise ValueError("Input value p out of domain")
 
-def PoissonPD():
-    pass
+def PoissonPD_calculate(k: int, p: Fraction) -> float:
+	return ((p ** k) / math.factorial(k)) * (math.e ** -p)
+
+def PoissonPD(kInput: int, pInput: float) -> float:
+	PoissonPD_validate(kInput, pInput)
+	return PoissonPD_calculate(kInput, Fraction(str(pInput)))
 
 
 
