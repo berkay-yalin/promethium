8da1a15714982ead54c237bc6cc43d50f78047b7
Stanislaw-Marc <126286170+Stanislaw-Marc@users.noreply.github.com>
2023-03-13 20:58:15 +0000

    NegativeBinomialPD function

--- python_probabilities/distributions_negative_binomial.py
+++ python_probabilities/distributions_negative_binomial.py
@@ -1,11 +1,22 @@
-def NegativeBinomialPD():
-	pass
+from decimal import Decimal
+import math
 
-def NegativeBinomialPD_validate():
-	pass
+def NegativeBinomialPD(xInput: int, rInput: int, pInput: float) -> Decimal:
+	NegativeBinomialPD_validate(xInput, rInput, pInput)
+	return NegativeBinomialPD_calculate(xInput, rInput, Decimal(str(pInput)))
 
-def NegativeBinomialPD_calculate():
-	pass
+def NegativeBinomialPD_validate(x: object, r: object, p: object) -> None:
+	if not isinstance(x, int) or x < 0:
+		raise TypeError("Input value x must be positive integer")
+	if not isinstance(r, int) or r < 0:
+		raise TypeError("Input value n must be positive integer")
+	if r > x:
+		raise TypeError("Number of successes must be less than trials")
+	if not isinstance(p, float) or not 0 <= p <= 1:
+		raise TypeError("Input value p must be a positive decimal percentage")
+
+def NegativeBinomialPD_calculate(x: int, r: int, p: Decimal) -> Decimal:
+	return math.comb(x-1, r-1) * (p**r) * ((1-p) ** (x-r))
 
 
 def NegativeBinomialCD():
