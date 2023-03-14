8809759dee98ce0c8c981a514d0e11702c9bbcfe
Stanisław Marć <126286170+stanislaw-marc@users.noreply.github.com>
2023-03-14 13:24:22 +0000

    InvNegativeBinomialCD function

--- python_probabilities/distributions_negative_binomial.py
+++ python_probabilities/distributions_negative_binomial.py
@@ -23,15 +23,27 @@ def NegativeBinomialCD(xInput: int, rInput: int, pInput: float) -> Decimal:
 	NegativeBinomialPD_validate(xInput, rInput, pInput)
 	return NegativeBinomialCD_calculate(xInput, rInput, pInput)
 
-def NegativeBinomialCD_calculate(x: int, r: int, p: Decimal):
+def NegativeBinomialCD_calculate(x: int, r: int, p: Decimal) -> Decimal:
 	return sum([NegativeBinomialPD(i, r, p) for i in range(r, x+1)])
 
 
-def InvNegativeBinomialCD():
-	pass
+def InvNegativeBinomialCD(areaInput: float, rInput: int, pInput: float) -> int:
+	InvNegativeBinomialCD_validate(areaInput, rInput, pInput)
+	return InvNegativeBinomialCD_calculate(areaInput, rInput, pInput)
 
-def InvNegativeBinomialCD_validate():
-	pass
+def InvNegativeBinomialCD_validate(area: object, r: object, p: object) -> None:
+	if not isinstance(area, float) or area < 0 or area > 1:
+		raise TypeError("Input value area must be a positive decimal precentage")
+	if not isinstance(r, int) or r < 0:
+		raise TypeError("Input value r must be positive integer")
+	if not isinstance(p, float) or not 0 <= p <= 1:
+		raise TypeError("Input value p must be a positive decimal precentage")
 
-def InvNegativeBinomialCD_calculate():
-	pass
+def InvNegativeBinomialCD_calculate(area: float, r: int, p: float) -> int:
+	cumulative = 0
+	i = r
+	while cumulative <= area:
+		cumulative += NegativeBinomialPD(i, r, p)
+		i += 1
+
+	return i - 1
