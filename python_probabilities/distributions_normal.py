6ba876eac520606d37b25863796e4528e6949f50
berkay-yalin <66202981+berkay-yalin@users.noreply.github.com>
2023-03-25 23:33:48 +0000

    replace decimal.Decimal with float
    Replaced decimal.Decimal datatype with float for calculations within NormalPD and NormalCD.

--- python_probabilities/distributions_normal.py
+++ python_probabilities/distributions_normal.py
@@ -1,42 +1,30 @@
-from decimal import Decimal
 from typing import Union, Any
-import math
-
-PI = Decimal(str(math.pi))
-EULER = Decimal(str(math.e))
+from math import erf, exp, sqrt, tau
+from statistics import _normal_dist_inv_cdf #type:ignore
 
 def NormalPD_validate(x: Any, mu: Any, sigma: Any) -> None:
 	if not isinstance(x, (int, float)):
 		raise TypeError("Input x value is invalid")
 	if not isinstance(mu, (int, float)):
 		raise TypeError("Input µ value is invalid")
-	if not sigma or not isinstance(sigma, (int, float)):
+	if (not sigma) or not isinstance(sigma, (int, float)):
 		raise TypeError("Input σ value is invalid")
 
-def NormalPD_calculate(x: Decimal, mu: Decimal, sigma: Decimal) -> Decimal:
-	lhs = (1/(sigma*((PI*2)**(Decimal('0.5')))))
-	rhs = (1/(EULER)**(((x-mu)**2)/(2*sigma**2)))
-	return(lhs*rhs)
+def NormalPD_calculate(x: float, mu: float, sigma: float) -> float:
+	variance = sigma * sigma
+	return exp((x - mu) ** 2 / (-2.0 * variance)) / sqrt(tau * variance)
 
 def NormalPD(x: Union[int, float], mu: Union[int, float], sigma: Union[int, float]) -> float:
 	NormalPD_validate(x, mu, sigma)
-	return float(NormalPD_calculate(
-		Decimal(str(x)),
-		Decimal(str(mu)),
-		Decimal(str(sigma))
-	))
+	return NormalPD_calculate(float(x), float(mu), float(sigma))
 
 
-def NormalCD_calculate(x: Decimal, mu: Decimal, sigma: Decimal) -> Decimal:
-	return Decimal('0.5')*(1+Decimal(str(math.erf((x-mu)/(sigma*(Decimal('2')**Decimal('0.5')))))))
+def NormalCD_calculate(x: float, mu: float, sigma: float) -> float:
+	return 0.5 * (1.0 + erf((x - mu) / (sigma * sqrt(2.0))))
 
 def NormalCD(x: Union[int, float], mu: Union[int, float], sigma: Union[int, float]) -> float:
 	NormalPD_validate(x, sigma, mu)
-	return float(NormalCD_calculate(
-		Decimal(str(x)),
-		Decimal(str(mu)),
-		Decimal(str(sigma))
-	))
+	return NormalCD_calculate(float(x), float(mu), float(sigma))
 
 
 def InvNormalCD_validate(p: Any, mu: Any, sigma: Any) -> None:
@@ -47,7 +35,6 @@ def InvNormalCD_validate(p: Any, mu: Any, sigma: Any) -> None:
 	if not sigma or not isinstance(sigma, (int, float)):
 		raise TypeError("Input σ value is invalid")
 
-from statistics import _normal_dist_inv_cdf # type:ignore
 def InvNormalCD(p: Union[int, float], mu: Union[int, float], sigma: Union[int, float]) -> float:
 	InvNormalCD_validate(p, mu, sigma)
 	return _normal_dist_inv_cdf(
