c960cf366456a98296dcfc673acbcacdd14234ea
berkay-yalin <66202981+berkay-yalin@users.noreply.github.com>
2023-03-26 23:48:29 +0100

    fix: rename ambiguous functions

--- python_probabilities/__init__.py
+++ python_probabilities/__init__.py
@@ -1,11 +1,3 @@
-from .distributions_binomial import BinomialPD as Bpd
-from .distributions_binomial import BinomialCD as Bcd
-from .distributions_binomial import InvBinomialCD as InvB
-
-from .distributions_poisson import PoissonPD as Ppd
-from .distributions_poisson import PoissonCD as Pcd
-from .distributions_poisson import InvPoissonCD as InvP
-
-from .distributions_normal import NormalPD as Npd
-from .distributions_normal import NormalCD as Ncd
-from .distributions_normal import InvNormalCD as InvN
+from .distributions_binomial import BinomialPD, BinomialCD, InvBinomialCD
+from .distributions_poisson import PoissonPD, PoissonCD, InvPoissonCD
+from .distributions_normal import NormalPD, NormalCD, InvNormalCD
