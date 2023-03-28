379ef2ab87aea79a3ddf521e560310fd53002d78
berkay-yalin <66202981+berkay-yalin@users.noreply.github.com>
2023-03-28 04:15:36 +0100

    fix: inaccurate binomial exceptions
    Validation functions for the binomial distributions inaccurately grouped together TypeErrors and ValueErrors.
    Fixed type annotations on validation function parameters.
    Parameters renamed to align with the rest of the codebase.

--- python_probabilities/distributions_binomial.py
+++ python_probabilities/distributions_binomial.py
@@ -1,38 +1,50 @@
+from typing import Any
 from decimal import Decimal
-import math
+from math import comb
 
+def BinomialPD_validate(x: Any, n: Any, p: Any) -> None:
+	if not isinstance(x, int):
+		raise TypeError("x must be a non-negative integer")
+	if x < 0:
+		raise ValueError("x value out of domain")
+	if not isinstance(n, int):
+		raise TypeError("n must be a non-negative integer")
+	if n < 0:
+		raise ValueError("n value out of domain")
+	if not isinstance(p, float):
+		raise TypeError("p must be non-negative float")
+	if not 0 <= p <= 1:
+		raise ValueError("p value out of domain")
 
-def BinomialPD_validate(k: object, n: object, p: object) -> None:
-	if not isinstance(k, int) or k < 0:
-		raise TypeError("Input value x must be positive integer")
-	if not isinstance(n, int) or n < 0:
-		raise TypeError("Input value n must be positive integer")
-	if not isinstance(p, float) or not 0 <= p <= 1:
-		raise TypeError("Input value p must be a positive decimal precentage")
+def BinomialPD_calculate(x: int, n: int, p: Decimal) -> Decimal:
+	return comb(n, x) * (p ** x) * ((1 - p) ** (n - x))
 
-def BinomialPD_calculate(k: int, n: int, p: Decimal) -> Decimal:
-	return math.comb(n, k) * (p ** k)  * ((1 - p) ** (n - k))
-
-def BinomialPD(kInput: int, nInput: int, pInput: float) -> Decimal:
-	BinomialPD_validate(kInput, nInput, pInput)
-	return BinomialPD_calculate(kInput, nInput, Decimal(str(pInput)))
+def BinomialPD(x: int, n: int, p: float) -> Decimal:
+	BinomialPD_validate(x, n, p)
+	return BinomialPD_calculate(x, n, Decimal(str(p)))
 
 
 def BinomialCD_calculate(k: int, n: int, p: Decimal) -> Decimal:
-	return sum([BinomialPD_calculate(i, n, p) for i in range(k + 1)])
+	return sum(BinomialPD_calculate(i, n, p) for i in range(k + 1))
 
-def BinomialCD(kInput: int, nInput: int, pInput: float) -> Decimal:
-	BinomialPD_validate(kInput, nInput, pInput)
-	return BinomialCD_calculate(kInput, nInput, Decimal(str(pInput)))
+def BinomialCD(x: int, n: int, p: float) -> Decimal:
+	BinomialPD_validate(x, n, p)
+	return BinomialCD_calculate(x, n, Decimal(str(p)))
 
 
-def InvBinomialCD_validate(x: object, n: object, p: object) -> None:
-	if not isinstance(x, float) or x < 0:
-		raise TypeError("Input value x must be a positive decimal precentage")
-	if not isinstance(n, int) or n < 0:
-		raise TypeError("Input value n must be positive integer")
-	if not isinstance(p, float) or not 0 <= p <= 1:
-		raise TypeError("Input value p must be a positive decimal precentage")
+def InvBinomialCD_validate(y: Any, n: Any, p: Any) -> None:
+	if not isinstance(y, float):
+		raise TypeError("y must be non-negative float")
+	if not 0 <= y <= 1:
+		raise ValueError("y value out of domain")
+	if not isinstance(n, int):
+		raise TypeError("n must be a non-negative integer")
+	if n < 0:
+		raise ValueError("n value out of domain")
+	if not isinstance(p, float):
+		raise TypeError("p must be non-negative float")
+	if not 0 <= p <= 1:
+		raise ValueError("p value out of domain")
 
 def InvBinomialCD_calculate(x: Decimal, n: int, p: Decimal) -> int:
 	for i in range(n + 1):
@@ -43,11 +55,6 @@ def InvBinomialCD_calculate(x: Decimal, n: int, p: Decimal) -> int:
 		if tempX > x:
 			return tempN
 
-def InvBinomialCD(xInput: float, nInput: int, pInput: float) -> int:
-	InvBinomialCD_validate(xInput, nInput, pInput)
-	return InvBinomialCD_calculate(
-		Decimal(str(xInput)),
-		nInput,
-		Decimal(str(pInput)),
-	)
-
+def InvBinomialCD(y: float, n: int, p: float) -> int:
+	InvBinomialCD_validate(y, n, p)
+	return InvBinomialCD_calculate(Decimal(str(y)), n, Decimal(str(p)))
