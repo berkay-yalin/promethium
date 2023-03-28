f427a991be5f013319971652998b01dcbec82800
Cillian2113 <98545522+Cillian2113@users.noreply.github.com>
2023-03-28 13:58:34 +0000

    test: geometric function testing

--- tests/test.py
+++ tests/test.py
@@ -1,53 +1,52 @@
 from python_probabilities import *
-GeometricPD
+from typing import Any
 
 geometricTestData = (
-	  {
-			'values': (6, 0.2435, 0.05),
-			'expected': {
-				  'Gpd': 0.06033143,
-				  'Gcd': 0.81256374,
-				  'InvG': 0.7138929909
-			}
-	  }
-
-	  {
-			'values': (50, 0.0567, 0.08),
-			'expected': {
-				  'Gpd': 0.06033143,
-				  'Gcd': 0.81256374,
-				  'InvG': 0.7138929909
-			}
-	  }
+	{
+		'values': (6, 0.2435, 0.05),
+		'expected': {
+				'Gpd': 0.06033143,
+				'Gcd': 0.81256374,
+				'InvG': 1
+		}
+	},
+	{
+		'values': (8, 0.3587, 0.8),
+		'expected': {
+				'Gpd': 0.01600147,
+				'Gcd': 0.97139184,
+				'InvG': 4
+		}
+	}
 )
 
-
 def GeometricTesting(function: str, testdata: Any):
-	  print(f"### TESTING {function.upper()} DISTRIBUTIONS ###")
-	  for i in testdata:
-			x = i['values'][0]
-			p = i['values'][1]`
-			area = i['values'][2]
-			if function == 'Gpd':
-				  observed: float = GeometricPD(x,p])
-				  expected: float = i['expected']['Gpd']
-			elif function == 'Gcd':
-				  observed: float = GeometricCD(*x,p)
-				  expected: float = i['expected']['Ncd']
-			elif function == 'InvG':
-				  observed: float = InvGeometricCD(area, p)
-				  expected: float = i['expected']['InvN']
-			else:
-				  raise SystemExit('what are you doing??')
+	print(f"### TESTING {function.upper()} DISTRIBUTIONS ###")
+	for i in testdata:
+		x = i['values'][0]
+		p = i['values'][1]
+		area = i['values'][2]
 
-			precision = len(str(expected).split('.')[-1])
+		if function == 'Gpd':
+				observed = GeometricPD(x, p)
+				expected= i['expected']['Gpd']
+		elif function == 'Gcd':
+				observed = GeometricCD(x, p)
+				expected = i['expected']['Gcd']
+		elif function == 'InvG':
+				observed = InvGeometricCD(area, p)
+				expected = i['expected']['InvG']
+		else:
+				raise SystemExit('what are you doing??')
 
-			if round(observed, precision) == expected:
+		precision = len(str(expected))
+		if str(observed)[:precision] == str(expected):
+			print(f"success!\n  {observed}\n≈ {expected}")
+		else:
+			print(f"fail! \n   {observed}\n!= {expected}")
+		print('### TESTING COMPLETE ###')
 
-				  print(f"success!\n  {observed}\n≈ {expected}")
-
-			else:
-
-				  print(f"fail! \n   {observed}\n!= {expected}")
-
-	  print('### TESTING COMPLETE ###')
+GeometricTesting('Gpd', geometricTestData)
+GeometricTesting('Gcd', geometricTestData)
+GeometricTesting('InvG', geometricTestData)
+GeometricTesting('Drake', geometricTestData)
